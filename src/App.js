b8993b0e59040551644405fbb512b69d009a8ef9
AKM Zahirul Islam <mamunjahir.com@gmail.com>
2021-04-22 12:18:53 +0600

    ra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {myvar: 1};
+  }
+  componentDidMount(){
+    console.log("Component Mounted")
+  }
+  componentDidUpdate(){
+    console.log("Component Updated")
+  }
+  onclickHandler=()=>{
+    this.setState({myvar:this.state.myvar+1})
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.myvar}</p>
+        <button onClick={()=>this.onclickHandler()}>Click</button>
+      </div>
+    )
+  }
 }
-
-export default App;
