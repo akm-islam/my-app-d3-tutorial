09ccd771ae686682852e3796150fa3aa7583a8a4
AKM Zahirul Islam <mamunjahir.com@gmail.com>
2021-04-30 12:45:06 +0600

    ra

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import * as d3 from "d3"
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {myvar: 1};
+    this.state = { myvar: 1 };
   }
-  componentDidMount(){
-   var my_selection=d3.selectAll("p")
-   console.log(my_selection)
+  componentDidMount() {
+    //-----------------------------------------------------------------------select and selectAll
+    //console.log(d3.select('circle')) // Selects only one circle. If there are multiple circles, the first one is returend
+    //console.log(d3.selectAll('circle')) // Selects all circles
+
+    //d3.select('circle').style('fill', 'orange')
+    //d3.selectAll('circle').style('fill', 'orange')
+
   }
-  componentDidUpdate(){
+  componentDidUpdate() {
     //console.log("Component Updated")
   }
-  onclickHandler=()=>{
-    this.setState({myvar:this.state.myvar+1})
+  onclickHandler = () => {
+    this.setState({ myvar: this.state.myvar + 1 })
   }
   render() {
     return (
-      <div className="App">
-        <p>{this.state.myvar}</p>
-        <button onClick={()=>this.onclickHandler()}>Click</button>
-      </div>
+      <svg width="760" height="140">
+        <circle r="40" cx="120" cy="50" className="circle1"/>
+        <circle r="40" cx="240" cy="50" />
+        <circle r="40" cx="360" cy="50" />
+        <circle r="40" cx="480" cy="50" />
+        <circle r="40" cx="600" cy="50" />
+      </svg>
     )
   }
 }
