5f271a802e228f592afb975e3d916f6dc9f8110a
AKM Zahirul Islam <mamunjahir.com@gmail.com>
2021-04-30 11:53:30 +0600

    ra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import './App.css';
+import * as d3 from "d3"
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {myvar: 1};
   }
   componentDidMount(){
-    console.log("Component Mounted")
+   var my_selection=d3.selectAll("p")
+   console.log(my_selection)
   }
   componentDidUpdate(){
-    console.log("Component Updated")
+    //console.log("Component Updated")
   }
   onclickHandler=()=>{
     this.setState({myvar:this.state.myvar+1})
